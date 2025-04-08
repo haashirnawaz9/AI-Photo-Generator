66f439c9a2cd3bfdd4b49d12dee321090b79b8e2
haashirnawaz9 <153224457+haashirnawaz9@users.noreply.github.com>
2025-04-08 18:04:28 -0400

    Update: Updated the ai photo generator page /main

---
+++ src/app/api/generate-image/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server'
+import { HfInference } from '@huggingface/inference'
+
+const hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
+
+export async function POST(req: Request) {
+    
+    try {
+        const { prompt } = await req.json();
+        const response = await hf.textToImage({
+            model: 'black-forest-labs/FLUX.1-schnell',
+            inputs: prompt,
+            parameters: {
+                guidance_scale: 0.0,
+                num_inference_steps: 4,
+                // max_sequence_length: 256,
+            },
+        });
+        console.log('Hugging Face API response received')
+
+        // Convert blob to base64
+        const buffer = await response.arrayBuffer();
+        const base64 = Buffer.from(buffer).toString('base64');
+        const dataUrl = `data:image/png;base64,${base64}`;
+
+        return NextResponse.json({ image: dataUrl });
+    } catch (error) {
+        console.error('Error generating image:', error);
+    }
+}
